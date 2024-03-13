772fab7c710ab0d519a353ff2735760f87df3d90
cleokibet <cleokibet@gmail.com>
2024-03-13 05:30:08 +0300

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/test/8-main.js
@@ -0,0 +1,5 @@
+bob@dylan:~$ cat 8-main.js
+import cleanSet from "./8-clean_set.js";
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
