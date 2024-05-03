7aa05b990b0594ac17804849123e654d200d85ec
cleokibet <cleokibet@gmail.com>
2024-05-03 05:42:30 +0300

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const cleophas = process.stdin.read();
+  const chunk = process.stdin.read();
 
   if (chunk) {
-    process.stdout.write(`Your name is: ${cleophas}`);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
